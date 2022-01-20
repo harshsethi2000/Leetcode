15eac87f0df3f5cf1100fa30b720f961f490dc49
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-01-20 07:31:37 +0530

    Update 876-middle-of-the-linked-list.java

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -10,6 +10,11 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
+         //Method 1
+        //simply iterate over Linkedlist and calculate the length of linked list
+        //now iterate over linked list and stop at n/2th node and return that node
+        
+        //Method 2-
         //using tortoise-Hare method
         //The intitution here is that fast will move 2 times faster than slow
         //so if fast becomes null that means our slow pointer is at n/2 distance from start
