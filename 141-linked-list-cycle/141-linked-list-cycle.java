ea9990d120c2648babaadb32c5202840e8232eff
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-01-21 07:56:48 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (40.61%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -16,14 +16,33 @@ public class Solution {
       //     if node already exist that means there is a cycle
       //         TIme COmplexity -O(n)
       //         Space Complexity -O(n)//for storing n nodes
-        HashSet<ListNode> hash=new HashSet<>();
-        while(head!=null)
-        {
-            if(hash.contains(head))return true;
-            hash.add(head);
-            head=head.next;
-        }
-        return false;
+//         HashSet<ListNode> hash=new HashSet<>();
+//         while(head!=null)
+//         {
+//             if(hash.contains(head))return true;
+//             hash.add(head);
+//             head=head.next;
+//         }
+//         return false;
               
+        // Method 2-
+        //     Using Cycle Detection Method Tortoise and hare
+        //     In this We will take two pointers slow and fast and increase the slow pointer by 1
+        //     and fast pointer by 2 if the slow and fast pointer collides at any point that means
+        //         there is cycle present
+        //         and if fast->next become null anytime that means there is no cycle
+        //             Time Complexity - O(n)
+        //             Space COmplexity -O(1)
+       
+        if(head==null)return false;
+        ListNode fast=head,slow=head;
+                    while(fast.next!=null && fast.next.next!=null)
+                    {
+                        
+                        slow=slow.next;
+                        fast=fast.next.next;
+                        if(slow==fast)return true;
+                    }
+        return false;
     }
 }
