4ada8c16f0009598bc85081eb0063c964140c084
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-01-28 14:37:23 +0530

    Time: 3 ms (85.15%), Space: 45 MB (8.41%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,36 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-//         We know that each element can be in the combination as many times
-//             So we can check for each element and will decide whether to choose that element or not,If we choose that element then we can again choose the same element.
-                
-    List<List<Integer>> ls=new ArrayList<>();
-        calculateCombinationSum(0,candidates,target,ls,new ArrayList<Integer>());
+        //taking a List to add all our unique subsequences 
+        List<List<Integer>> ls=new ArrayList<>();
+        calculateSum(0,candidates,target,ls,new ArrayList<Integer>());
         return ls;
     }
-    public void calculateCombinationSum(int index,int [] candidates,int target,List<List<Integer>> ls,ArrayList<Integer> ds)
+    public void calculateSum(int index,int[] nums, int target,List<List<Integer>> ls,List<Integer>ds)
     {
-        //Base condition
-        if(index==candidates.length)
+        
+        //base condition
+        if(index==nums.length)
         {
-            if(target==0)
-            {
-                ls.add(new ArrayList<Integer>(ds));
-            }
-            return;
+        if(target==0)    ls.add(new ArrayList<Integer>(ds));
+         return;
         }
-        //select that element
-        if(candidates[index]<=target)
+        //we will pick a element if it is less than our target
+        if(target>=nums[index])
         {
-            //if we are selecting that element then add it in ds
-          ds.add(candidates[index]);
-          calculateCombinationSum(index,candidates,target-candidates[index],ls,ds);
-          //remove added element from ds
+            ds.add(nums[index]);
+            //we can choose the same number twice soo no need to increment index
+            calculateSum(index,nums,target-nums[index],ls,ds);
             ds.remove(ds.size()-1);
         }
-        
-        //not selecting that element soo no need to change the target
-        calculateCombinationSum(index+1,candidates,target,ls,ds);
-        
+        calculateSum(index+1,nums,target,ls,ds);
     }
-    
 }
