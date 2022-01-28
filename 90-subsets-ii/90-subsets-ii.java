9c67e7813e7c56fcb5e1ce9b2fcbf170a342905b
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-01-28 14:18:27 +0530

    Time: 3 ms (42.97%), Space: 44.5 MB (7.02%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        //arrayList to insert all the unique subsets
         List<List<Integer>> ls=new ArrayList<>();
         Arrays.sort(nums);
-        calculateSubset(0,nums,ls,new ArrayList<Integer>());
+        calculateSubsets(0,nums,ls,new ArrayList<Integer>());
         return ls;
     }
-    public void calculateSubset(int index,int[] nums,List<List<Integer>> ls,List<Integer> ds)
+    public void calculateSubsets(int index,int [] nums,List<List<Integer>> ls,List<Integer> ds)
     {
+        //Base condition
+        //add our ds in our final ls
         ls.add(new ArrayList<Integer>(ds));
         for(int i=index;i<nums.length;i++)
         {
-            if(i!=index && nums[i]==nums[i-1])continue;
+            //to eliminate the dulicates
+           if(i!=index && nums[i]==nums[i-1])continue;
             ds.add(nums[i]);
-            calculateSubset(i+1,nums,ls,ds);
+            calculateSubsets(i+1,nums,ls,ds);
+            //after backtracking will remove that element from our DS
             ds.remove(ds.size()-1);
         }
-        
-        
     }
 }
