c1870cc2b7845a700941c37641a54836f50aa5a0
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-02-12 13:40:01 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (6.04%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -22,21 +22,68 @@
 //     TC - O(n)
 //     SC - O(n)+O(n)
     
+// class Solution {
+//     public int rob(int[] nums) {
+//         int dp[]=new int[nums.length];
+//         Arrays.fill(dp,-1);
+//         return getMaximumSum(nums.length-1,nums,dp);
+//     }
+//     public int getMaximumSum(int index,int [] nums,int []dp)
+//     {
+//         if(index==0)return nums[index];
+//         if(index<0)return 0;
+//         if(dp[index]!=-1)return dp[index];
+//         int pick=nums[index]+ getMaximumSum(index-2,nums,dp);
+//         int notPick=getMaximumSum(index-1,nums,dp);
+//         dp[index]=Math.max(pick,notPick);
+//         return dp[index];
+//     }
+        
+// }
+
+// Method 3-
+//     Using Tabulation
+//     SC - O(n)
+//     TC -O(n)
+    
+// class Solution {
+//     public int rob(int[] nums) {
+//         int dp[]=new int[nums.length];
+//         Arrays.fill(dp,-1);
+//         dp[0]=nums[0];
+//         for(int i=1;i<nums.length;i++)
+//         {
+//             int pick=nums[i];
+//             if(i>1)pick+=dp[i-2];
+//             int notPick=dp[i-1];
+//             dp[i]=Math.max(pick,notPick);
+//         }
+//         return dp[nums.length-1];
+
+//    }
+        
+// }
+
+// Method 4 -
+//     Optimized Tabulation
+//     TC - O(n)
+//     SC -O(1)
 class Solution {
     public int rob(int[] nums) {
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return getMaximumSum(nums.length-1,nums,dp);
-    }
-    public int getMaximumSum(int index,int [] nums,int []dp)
-    {
-        if(index==0)return nums[index];
-        if(index<0)return 0;
-        if(dp[index]!=-1)return dp[index];
-        int pick=nums[index]+ getMaximumSum(index-2,nums,dp);
-        int notPick=getMaximumSum(index-1,nums,dp);
-        dp[index]=Math.max(pick,notPick);
-        return dp[index];
-    }
+        
+        int prev=nums[0];
+        int secPrev=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            int pick=nums[i]+secPrev;
+            int notPick=prev;
+            int curr=Math.max(pick,notPick);
+            secPrev=prev;
+            prev=curr;
+        }
+        return prev;
+
+   }
         
 }
+
