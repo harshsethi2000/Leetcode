76aaf97b0e9ef0bf2aa6fa99f287e6bd460a4161
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-03-24 10:33:49 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (76.32%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,20 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Iterative Solution
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
        vector<int> v;
         if(root==NULL)return v;
-        traversal(root,v);
+        stack<TreeNode *> s;
+        for(TreeNode *t=root;t!=NULL;t=t->left)s.push(t);
+        while(!s.empty())
+        {
+         TreeNode *tmp=s.top();
+            s.pop();
+            v.push_back(tmp->val);
+            if(tmp->right!=NULL)
+            {
+                for(TreeNode *t=tmp->right;t!=NULL;t=t->left)s.push(t);        
+            }
+        }
         return v;
         
     }
-    void traversal(TreeNode * root,vector<int> &v)
-    {
-        if(root==NULL)return;
-        traversal(root->left,v);
-        v.push_back(root->val);
-        traversal(root->right,v);
-    }
+   
 };
+//Method 2-
+// Recursive Solution
+
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//        vector<int> v;
+//         if(root==NULL)return v;
+//         traversal(root,v);
+//         return v;
+        
+//     }
+//     void traversal(TreeNode * root,vector<int> &v)
+//     {
+//         if(root==NULL)return;
+//         traversal(root->left,v);
+//         v.push_back(root->val);
+//         traversal(root->right,v);
+//     }
+// };
