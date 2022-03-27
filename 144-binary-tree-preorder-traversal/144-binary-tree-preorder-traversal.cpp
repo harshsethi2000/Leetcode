f9fbf661e52fc82d0da88e276f9c7fa7c43098f9
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-03-27 11:51:24 +0530

    Time: 6 ms (14.94%), Space: 8.4 MB (44.38%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -9,43 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     vector<int> preorderTraversal(TreeNode* root) {
-//         vector<int> ans;
-//         if(root==NULL)return ans;
-//         preorder(root,ans);
-//         return ans;
-//     }
-//     void preorder(TreeNode *root,vector<int> &ans)
-//     {
-//         if(root==NULL)return;
-//         ans.push_back(root->val);
-//         preorder(root->left,ans);
-//         preorder(root->right,ans);
-//     }
-// };
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
         if(root==NULL)return ans;
-        stack<TreeNode *> s;
-        s.push(root);
-        while(!s.empty())
-        {
-            TreeNode *t=s.top();
-            s.pop();
-            ans.push_back(t->val);
-            if(t->right)
-            {
-                s.push(t->right);
-            }
-            if(t->left)
-            {
-                s.push(t->left);
-            }
-        }
+        preorder(root,ans);
         return ans;
     }
-};
+    void preorder(TreeNode *root,vector<int> &ans)
+    {
+        if(root==NULL)return;
+        ans.push_back(root->val);
+        preorder(root->left,ans);
+        preorder(root->right,ans);
+    }
+ };
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         if(root==NULL)return ans;
+//         stack<TreeNode *> s;
+//         s.push(root);
+//         while(!s.empty())
+//         {
+//             TreeNode *t=s.top();
+//             s.pop();
+//             ans.push_back(t->val);
+//             if(t->right)
+//             {
+//                 s.push(t->right);
+//             }
+//             if(t->left)
+//             {
+//                 s.push(t->left);
+//             }
+//         }
+//         return ans;
+//     }
+// };
