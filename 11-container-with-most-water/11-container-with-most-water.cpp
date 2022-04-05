362eb1f648776032928eca61ab81057240bf681d
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-04-05 23:02:28 +0530

    Time: 209 ms (5.66%), Space: 59 MB (38.72%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -0,0 +1,35 @@
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int ans=0;
+//         for(int i=0;i<height.size()-1;i++)
+//         {
+            
+//             for(int j=i+1;j<height.size();j++)
+//             {
+//                 int k=min(height[i],height[j]);
+                
+//                 ans=max(ans,k*(j-i));
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans=0;
+        int i=0,j=height.size()-1;
+        while(i<j)
+        {
+            int k=min(height[i],height[j]);
+            ans=max(ans,k*(j-i));
+            if(height[i]<height[j])i++;
+            else j--;
+            
+        }
+        return ans;
+    }
+};
+
