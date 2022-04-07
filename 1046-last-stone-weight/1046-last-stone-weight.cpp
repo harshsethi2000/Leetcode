8e2742375b251f9983a1a9b08899e5807f5d90d5
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-04-07 10:57:09 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,30 +1,48 @@
+// class Solution {
+// public:
+//     int lastStoneWeight(vector<int>& stones) {
+//         if(stones.size()==1)return stones[0];
+//         sort(stones.begin(),stones.end());
+        
+//         while(stones.size()!=0 || stones.size()!=1)
+//         {
+//             int firstMaxIndex=stones.size()-1;
+//             int secondMaxIndex=stones.size()-2;
+//             if(stones[secondMaxIndex]==stones[firstMaxIndex])
+//             {
+//                 //remove both
+//                 stones.erase(stones.begin()+firstMaxIndex);
+//                 stones.erase(stones.begin()+secondMaxIndex);
+//             }
+//             else if(stones[secondMaxIndex]<stones[firstMaxIndex])
+//             {
+               
+//                 stones[firstMaxIndex]-=stones[secondMaxIndex];
+//                 stones.erase(stones.begin()+secondMaxIndex);
+//             }
+//             if(stones.size()==0|| stones.size()==1)break;
+//             sort(stones.begin(),stones.end());
+//         }
+//         sort(stones.begin(),stones.end());
+//         if(stones.size()==0)return 0;
+//         return stones[0];
+//         }
+// };
+
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stones) {
-        if(stones.size()==1)return stones[0];
-        sort(stones.begin(),stones.end());
-        
-        while(stones.size()!=0 || stones.size()!=1)
+ int lastStoneWeight(vector<int>& stones) 
+    {
+        priority_queue<int> pq(stones.begin(),stones.end());
+        while(pq.size()>1)
         {
-            int firstMaxIndex=stones.size()-1;
-            int secondMaxIndex=stones.size()-2;
-            if(stones[secondMaxIndex]==stones[firstMaxIndex])
-            {
-                //remove both
-                stones.erase(stones.begin()+firstMaxIndex);
-                stones.erase(stones.begin()+secondMaxIndex);
-            }
-            else if(stones[secondMaxIndex]<stones[firstMaxIndex])
-            {
-               
-                stones[firstMaxIndex]-=stones[secondMaxIndex];
-                stones.erase(stones.begin()+secondMaxIndex);
-            }
-            if(stones.size()==0|| stones.size()==1)break;
-            sort(stones.begin(),stones.end());
+            int y=pq.top();
+            pq.pop();
+            int x=pq.top();
+            pq.pop();
+            if(x!=y) pq.push(y-x);
         }
-        sort(stones.begin(),stones.end());
-        if(stones.size()==0)return 0;
-        return stones[0];
-        }
+        return pq.empty()? 0 : pq.top();
+    }
 };
+
