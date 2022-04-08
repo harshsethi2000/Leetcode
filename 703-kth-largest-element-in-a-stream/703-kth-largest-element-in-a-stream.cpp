7540b5d52527991399cf48d2fe7d7dcac5b9b89b
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-04-08 11:22:45 +0530

    Time: 45 ms (53.48%), Space: 19.9 MB (17.78%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,9 +1,20 @@
+
+// Using Priority Queue -
+//     We will be using priority queue and will add the K elements in our queue
+//     Since priority queue in C++ follow the max-heap soo we will add negative of every 
+//     element to make it follow the min-heap conditions.
+
+// N as the length of nums and M as the number of calls to add(),
+// Time Complexity: O(N⋅log(N)+M⋅log(k))    
+// space complexity: O(N)O(N)
+    
+
 class KthLargest {
 public:
     priority_queue<int> p;
     int k;
     KthLargest(int K, vector<int>& nums) {
-        k=K;
+        this->k=K;
         for(int i=0;i<nums.size();i++)
         {
             p.push(-nums[i]);
