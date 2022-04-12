59709a25f225e0b611e32c1e928e008143a569e5
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-04-12 22:55:56 +0530

    Time: 1 ms (55.24%), Space: 43.3 MB (7.06%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,44 +1,28 @@
 class Solution {
-   public void gameOfLife(int[][] board) {
-		int m = board.length;
-		int n = board[0].length;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				checkAndUpdate(board, i, j);
-			}
-		}
-		// update board to change 2 to 1 and -1 to 0 back.
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				// board[i][j] will be 1 if value is either 1 or 2. else it will be 0
-				board[i][j] = board[i][j] >= 1 ? 1 : 0;
-			}
-		}
-	}
+public void gameOfLife(int[][] board) {
+    int rows = board.length, cols = board[0].length;
+    int[][] tempBoard = new int[rows][cols];                                             // we will first update values in this temporary matrix
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            int liveNeighbors = neighbor(board, r-1, c-1) + neighbor(board, r-1, c) + neighbor(board, r-1, c+1) + neighbor(board, r, c+1) + 
+                                neighbor(board, r+1, c+1) + neighbor(board, r+1, c) + neighbor(board, r+1, c-1) + neighbor(board, r, c-1);
+            //System.out.println("cell: ("+r+","+c+") ,  liveNeighbors: "+liveNeighbors);
+            if(board[r][c] == 1){ 
+                tempBoard[r][c] = (liveNeighbors < 2 || liveNeighbors > 3) ? 0 : 1;      // update temporary matrix, based on the rules in question
+            }else{ 
+                tempBoard[r][c] = (liveNeighbors == 3) ? 1 : 0;
+            }
+        }
+    }
+    
+    for(int r = 0; r < rows; r++){
+        board[r] = tempBoard[r].clone();                                                // copy all tempBoard elements back to board matrix
+    }
+}
 
-	private void checkAndUpdate(int[][] board, int i, int j) {
-		int m = board.length;
-		int n = board[0].length;
-		int count = 0;
-		int[][] indexes = { { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
-		// try every possible direction and count no of 1 or -1.
-		for (int[] index : indexes) {
-			if (i + index[0] < 0 || i + index[0] >= m || j + index[1] < 0 || j + index[1] >= n) {
-				continue;
-			}
-			// if neighbor is either 1 or -1. Increment count.
-			if (Math.abs(board[i + index[0]][j + index[1]]) == 1) {
-				count++;
-			}
-		}
-		// if current board element is 0 and count 3 it means it's new value will be 1
-		// in future. hence make it 2.
-		if (board[i][j] == 0 && count == 3) {
-			board[i][j] = 2;
-		} else if (board[i][j] == 1 && (count < 2 || count > 3)) {
-			// if current element is 1 and count is <2 or > 3. it means it's new value will
-			// be 0 in future.hence make it -1.
-			board[i][j] = -1;
-		}
-	}
+// ----------------------------------------------------------------------- //
+public int neighbor(int[][] board, int r, int c){
+    if( r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] == 0 ){ return 0; }                          // out of bound cases
+    return 1;
+}
 }
