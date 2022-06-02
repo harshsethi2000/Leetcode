a53f2aedd29dfa1af5b7941e5e6bd717c8161def
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-06-02 21:57:22 +0530

    Time: 3 ms (76.56%), Space: 8.4 MB (93.96%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int sum=0;
+        for(auto num:nums)
+        {
+            sum+=num;
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
