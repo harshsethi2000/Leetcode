cfa4135d4eefee27206f74f24ad72b97ac72e290
Harsh Sethi <57841517+harshsethi2000@users.noreply.github.com>
2022-08-20 21:50:39 +0530

    Time: 19 ms (63.20%), Space: 20.8 MB (95.11%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -16,23 +16,43 @@ class Solution {
     //Naive approach
     //for each node we will find leftChildHeight and rightChild Height
     //and will compare if for any (rch-lch)>1 return false
-    
+    // TC : O(N*N) since for every call we are calling getHeight function as well
+    // SC : stackSpace
 public:
-    int getHeight(TreeNode * root){
-        if(root == NULL)return 0;
-        return max(getHeight(root->left), getHeight(root->right)) + 1;
+    // int getHeight(TreeNode * root){
+    //     if(root == NULL)return 0;
+    //     return max(getHeight(root->left), getHeight(root->right)) + 1;
+    // }
+    // bool isBalanced(TreeNode* root) {
+    //     if(root == NULL)
+    //             return true;
+    //     int lh = getHeight(root->left);
+    //     int rh = getHeight(root->right);
+    //     if(abs(rh - lh) > 1)
+    //         return false;
+    //     bool la = isBalanced(root->left);
+    //     bool ra = isBalanced(root->right);
+    //     if(!la || !ra)
+    //         return false;
+    //     return true;
+    // }
+    //now as we can see in the above solution for each call we are redundantly 
+    //calling getHeight 
+    //TC : O(N)
+     bool isBalanced(TreeNode* root) {
+       return helper(root)!=-1;
     }
-    bool isBalanced(TreeNode* root) {
+    int helper(TreeNode * root){
         if(root == NULL)
-                return true;
-        int lh = getHeight(root->left);
-        int rh = getHeight(root->right);
-        if(abs(rh - lh) > 1)
-            return false;
-        bool la = isBalanced(root->left);
-        bool ra = isBalanced(root->right);
-        if(!la || !ra)
-            return false;
-        return true;
+            return 0;
+        int lh = helper(root->left);
+        int rh = helper(root->right);
+        if(lh == -1 || rh == -1)
+            return -1;
+        if(abs(lh - rh)>1)
+            return -1;
+        
+        return max(lh,rh)+1;
     }
+
 };
